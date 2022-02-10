f12e5ac8b4c4ad1e55ec38a956bfdb966d0cfe2f
os3ketchup <eldorteshaboyev1994@gmail.com>
2022-02-10 18:56:55 +0500

    DAS

--- app/src/main/java/com/eldorteshaboev/mypageradapter/MainActivity.kt
+++ app/src/main/java/com/eldorteshaboev/mypageradapter/MainActivity.kt
@@ -2,25 +2,28 @@ package com.eldorteshaboev.mypageradapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.eldorteshaboev.mypageradapter.adapter.MyPagerAdapter
+import com.eldorteshaboev.mypageradapter.adapter.MyFragmentPageAdapter
 import com.eldorteshaboev.mypageradapter.databinding.ActivityMainBinding
+import com.eldorteshaboev.mypageradapter.models.User
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var list: ArrayList<String>
-    lateinit var myPagerAdapter: MyPagerAdapter
+    lateinit var list: ArrayList<User>
+    lateinit var myFragmentPageAdapter: MyFragmentPageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         list = ArrayList()
-        list.add("https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg")
-        list.add("https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg")
-        list.add("https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg")
+        list.add(User(imageLink = "https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg", name = "Test1"))
+        list.add(User(imageLink = "https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg", name = "Test2"))
+        list.add(User(imageLink = "https://storage.kun.uz/source/thumbnails/_medium/8/XaRTF01ZC8vWszd5grBqrE9H74JjTIGn_medium.jpg", name = "Test3"))
 
+            myFragmentPageAdapter = MyFragmentPageAdapter(list,supportFragmentManager)
+            binding.myViewPager.adapter = myFragmentPageAdapter
 
-        myPagerAdapter = MyPagerAdapter(list)
-        binding.myViewPager.adapter = myPagerAdapter
+            binding.tabLayout.setupWithViewPager(binding.myViewPager)
+
     }
 }
